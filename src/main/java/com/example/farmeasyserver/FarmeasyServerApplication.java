691bad2bd21d86bed9f5ae5b4d2fa826d17b9411
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-03-18 20:57:07 +0900

    Add files via upload

---
+++ src/main/java/com/example/farmeasyserver/FarmeasyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmeasyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmeasyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmeasyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/farmeasyserver/FarmeasyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmeasyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmeasyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
