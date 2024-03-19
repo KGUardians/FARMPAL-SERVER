7fadfd1668db7d3b7e694c802e947b8775f33963
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-03-19 10:22:17 +0900

    123

---
+++ src/main/java/com/example/farmeasyserver/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.farmeasyserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String LoginPage(){
+        return "login";
+    }
+}
