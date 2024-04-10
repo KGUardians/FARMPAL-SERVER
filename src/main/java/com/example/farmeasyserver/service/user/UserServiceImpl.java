ecbff69d4b0988ec8270686a00b856b94bcacad8
YJYJ1 <127469107+YJYJ1@users.noreply.github.com>
2024-04-10 21:24:30 +0900

    User service, repository 수정

--- src/main/java/com/example/farmeasyserver/service/user/UserServiceImpl.java
+++ src/main/java/com/example/farmeasyserver/service/user/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.farmeasyserver.service.user;
 
+import com.example.farmeasyserver.repository.UserRepository;
 import com.example.farmeasyserver.repository.jpa.UserJpaRepository;
-import com.example.farmeasyserver.repository.user.UserRepository;
 import com.example.farmeasyserver.entity.user.User;
 import lombok.RequiredArgsConstructor;
 
@@ -10,16 +10,16 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
     private final UserJpaRepository userJpaRepository;
+    private final UserRepository UserRepository;
 
     @Override
     public void join(User user) {
-        userRepository.save(user);
+        UserRepository.save(user);
     }
 
     @Override
-    public User findUser(String username) {
-        return userJpaRepository.findUserByUsername(username);
+    public Optional<User> findUser(String username) {
+        return UserRepository.findByname(username);
     }
 }
