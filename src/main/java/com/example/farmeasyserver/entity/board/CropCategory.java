c6b4a5ada4cee305b2c6d167006ae369a83ed79c
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-04-29 14:44:58 +0900

    Create CropCategory.java

---
+++ src/main/java/com/example/farmeasyserver/entity/board/CropCategory.java
@@ -0,0 +1,17 @@
+package com.example.farmeasyserver.entity.board;
+
+import jakarta.persistence.Enumerated;
+
+public enum CropCategory {
+    STRAWBERRY,
+    CARROT,
+    GRAPE,
+    APPLE,
+    ORANGE,
+    LETTUCE,
+    POTATO,
+    TOMATO,
+    EGGPLANT,
+    CUCUMBER,
+    OTHER
+}
