d487cc761fccbe3f353980909f4e540f80c4617b
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-13 23:33:24 +0900

    이름으로 유저찾기 구현

--- src/main/java/com/example/farmeasyserver/repository/UserJpaRepo.java
+++ src/main/java/com/example/farmeasyserver/repository/UserJpaRepo.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface UserJpaRepo extends JpaRepository<User,Long> {
 
-
+    Optional<User> findByUsername(String username);
     @Query("select u from User u where u.username = :userName and u.phoneNumber = :phoneNumber")
     Optional<User> findByUsernameAndPhoneNumber(String userName, String phoneNumber);
 
