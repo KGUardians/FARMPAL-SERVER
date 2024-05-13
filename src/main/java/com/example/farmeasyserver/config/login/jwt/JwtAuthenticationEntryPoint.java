287c21e60f42740792d9d9f3140b8c3487f1b680
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-13 23:36:22 +0900

    jwt authenticationentrtpoint ㄱ ㅜ현

--- src/main/java/com/example/farmeasyserver/config/login/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/farmeasyserver/config/login/jwt/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,21 @@
 package com.example.farmeasyserver.config.login.jwt;
 
-public class JwtAuthenticationEntryPoint {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // 유효한 자격증명을 제공하지 않고 접근하려 할때, 401상태코드 반환
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
