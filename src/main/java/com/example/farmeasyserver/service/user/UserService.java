f8c5b70b0967e704c56182cec095249ff224d057
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-13 23:34:08 +0900

    회원 로그인 구현

--- src/main/java/com/example/farmeasyserver/service/user/UserService.java
+++ src/main/java/com/example/farmeasyserver/service/user/UserService.java
@@ -1,10 +1,14 @@
 package com.example.farmeasyserver.service.user;
 
 import com.example.farmeasyserver.dto.user.JoinUserForm;
+import com.example.farmeasyserver.dto.user.LoginRequest;
 import com.example.farmeasyserver.dto.user.UserDto;
+import com.example.farmeasyserver.dto.user.UserTokenDto;
 
 
 public interface UserService {
     UserDto join (JoinUserForm user);
+
+    UserTokenDto signIn(LoginRequest req);
 
 }
