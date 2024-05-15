3dbfc951dfb92c2c7cddc116a8025121ac701df9
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-15 19:47:49 +0900

    createFarm 이름 변경
    create -> Register

--- src/main/java/com/example/farmeasyserver/dto/user/RegisterFarmReq.java
+++ src/main/java/com/example/farmeasyserver/dto/user/RegisterFarmReq.java
@@ -5,12 +5,12 @@ import com.example.farmeasyserver.entity.user.Farm;
 import lombok.Data;
 
 @Data
-public class CreateFarmReq {
+public class RegisterFarmReq {
     private Long userId;
     private String farmName;
     private Address farmAddress;
 
-    public static Farm toEntity(CreateFarmReq req){
+    public static Farm toEntity(RegisterFarmReq req){
         return new Farm(req.getFarmName(),req.farmAddress);
     }
 }
