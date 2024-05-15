31539739d0e593e2979ee6eed2d5d626086c657c
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-15 19:11:39 +0900

    create createFarmReq

--- src/main/java/com/example/farmeasyserver/dto/user/createFarmReq.java
+++ src/main/java/com/example/farmeasyserver/dto/user/createFarmReq.java
@@ -1,4 +1,11 @@
 package com.example.farmeasyserver.dto.user;
 
+import com.example.farmeasyserver.entity.user.Address;
+import lombok.Data;
+
+@Data
 public class createFarmReq {
+    private Long userId;
+    private String farmName;
+    private Address farmAddress;
 }
