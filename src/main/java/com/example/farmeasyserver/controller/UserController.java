2cbd63d2703e0e85cc5f2fe074d6042ddc50cf0a
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-15 19:05:18 +0900

    user controller login 이름 변경
    login -> signIn

--- src/main/java/com/example/farmeasyserver/controller/UserController.java
+++ src/main/java/com/example/farmeasyserver/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.farmeasyserver.controller;
 import com.example.farmeasyserver.dto.user.JoinUserForm;
 import com.example.farmeasyserver.dto.response.Response;
 import com.example.farmeasyserver.dto.user.LoginRequest;
+import com.example.farmeasyserver.entity.board.Post;
+import com.example.farmeasyserver.entity.board.PostType;
 import com.example.farmeasyserver.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +15,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    @GetMapping("/join")
-    public void joinForm(){
-
-    }
 
     @PostMapping("/join")
     public Response joinUser(@RequestBody @Valid JoinUserForm form){
         return Response.success(userService.join(form));
     }
 
-    @PostMapping("/login")
-    public Response login(@RequestBody LoginRequest req){
+    @PostMapping("/sign-in")
+    public Response signIn(@RequestBody LoginRequest req){
         return Response.success(userService.signIn(req));
     }
+
 }
