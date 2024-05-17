5e6e35d7d3290e75f0ce80f49fa6f2d93c31b948
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-17 11:42:06 +0900

    에러 메세지 내용 변경

--- src/main/java/com/example/farmeasyserver/config/login/auth/CustomUserDetailsService.java
+++ src/main/java/com/example/farmeasyserver/config/login/auth/CustomUserDetailsService.java
@@ -14,6 +14,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserJpaRepo userJpaRepo;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userJpaRepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("NOT FOUND"));
+        return userJpaRepo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("USERNAME NOT FOUND"));
     }
 }
