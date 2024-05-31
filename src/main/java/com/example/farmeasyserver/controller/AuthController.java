c7fdbc9cdc084eaf1bc722e8c6c018a773df1b96
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-31 16:09:54 +0900

    refreshToken api 구현

--- src/main/java/com/example/farmeasyserver/controller/AuthController.java
+++ src/main/java/com/example/farmeasyserver/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.farmeasyserver.controller;
 
+import com.example.farmeasyserver.config.login.jwt.JwtProperties;
 import com.example.farmeasyserver.dto.user.JoinUserReq;
 import com.example.farmeasyserver.dto.response.Response;
 import com.example.farmeasyserver.dto.user.LoginReq;
@@ -7,6 +8,7 @@ import com.example.farmeasyserver.dto.user.RegisterFarmReq;
 import com.example.farmeasyserver.entity.user.User;
 import com.example.farmeasyserver.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final JwtProperties jwtProperties;
 
     @PostMapping("/sign-up")
     @Operation(summary = "유저 회원가입 요청")
@@ -35,6 +38,13 @@ public class AuthController {
     public Response registerFarm(@RequestBody RegisterFarmReq req){
         User user = userService.findByUsername();
         return Response.success(userService.createFarm(req, user));
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "refresh token 생성 요청")
+    public Response requestRefreshToken(HttpServletRequest request){
+        String refreshToken = jwtProperties.extractTokenFromHeader(request);
+        return Response.success(userService.refreshToken(refreshToken));
     }
 
 }
