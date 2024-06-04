b193f9581548c7ae4c385a9b37e2299adb57dd17
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-06-04 10:23:01 +0900

    Update SwaggerConfig.java

--- src/main/java/farmeasy/server/config/SwaggerConfig.java
+++ src/main/java/farmeasy/server/config/SwaggerConfig.java
@@ -3,15 +3,31 @@ package farmeasy.server.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Array;
+
 @Configuration
 public class SwaggerConfig {
+
+    public SecurityScheme securityScheme() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        return securityScheme;
+    }
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme().getName(),securityScheme())
+                )
                 .info(apiInfo());
     }
 
